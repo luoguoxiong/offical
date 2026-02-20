dd5790e030510bc3e395f6b9ac662a1c80c2ac8e
luoguoxiong <luoguoxiong@codemao.cn>
2026-02-20 09:40:25 +0800

    初始化项目

--- .next/dev/server/chunks/ssr/[root-of-the-server]__e176a675._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__e176a675._.js
@@ -431,7 +431,7 @@ function Providers({ children }) {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2d$themes$2f$dist$2f$index$2e$module$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["ThemeProvider"], {
         attribute: "class",
         enableSystem: false,
-        defaultTheme: "dark",
+        defaultTheme: "light",
         children: children
     }, void 0, false, {
         fileName: "[project]/src/app/providers.tsx",
--- .next/dev/static/chunks/[root-of-the-server]__54ad8d83._.js
+++ .next/dev/static/chunks/[root-of-the-server]__54ad8d83._.js
@@ -443,7 +443,7 @@ function Providers({ children }) {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2d$themes$2f$dist$2f$index$2e$module$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["ThemeProvider"], {
         attribute: "class",
         enableSystem: false,
-        defaultTheme: "dark",
+        defaultTheme: "light",
         children: children
     }, void 0, false, {
         fileName: "[project]/src/app/providers.tsx",
